16f804f3c24e34d21a20a7bf546408f41dd01d80
arigsela <arigsela@gmail.com>
2023-09-01 12:19:19 -0400

    Adding argocd and projects

---
+++ terraform/modules/argocd/versions.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_providers {
+   aws = "~> 5.0"
+   helm = "~> 2.10.0"
+   kubernetes = "2.23.0"
+   kubectl = {
+    source = "gavinbunney/kubectl"
+    version = "1.14.0"
+   }
+  }
+}
--- terraform/roots/asela-cluster/argocd.tf
+++ terraform/roots/asela-cluster/argocd.tf
@@ -1,30 +1,5 @@
-resource "helm_release" "argocd" {
-  depends_on = [var.mod_dependency, kubernetes_namespace.argocd]
-  count      = var.enabled ? length(var.helm_services) : 0
-  name       = var.helm_services[count.index].name
-  chart      = var.helm_services[count.index].release_name
-  repository = var.helm_chart_repo
-  version    = var.helm_services[count.index].chart_version
-  namespace  = var.namespace
-
-  set {
-    name  = "installCRDs"
-    value = true
-  }
-
-  set {
-      name = "server.service.type"
-      value = "NodePort"
-  }
-
-  set {
-    name  = "server.config.exec\\.enabled"
-    value = "true"
-    type  = "string"
-  }
-  
-  values = [
-    yamlencode(var.helm_services[count.index].settings),
-    yamlencode(var.settings)
-  ]
+module "argocd" {
+  source = "../../modules/argocd"
+  enabled = true
+  namespace = "argo-cd"
 }
