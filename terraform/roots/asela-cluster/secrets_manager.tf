1155e0030a07ae3b83b07a9363afcf8714f17f7b
arigsela <arigsela@gmail.com>
2023-09-19 10:55:21 -0400

    adding secrets in terraform

--- terraform/roots/asela-cluster/secrets_manager.tf
+++ terraform/roots/asela-cluster/secrets_manager.tf
@@ -8,6 +8,10 @@ resource "aws_secretsmanager_secret" "rds_west2_beappdb1_credentials" {
     name = "rds-us-west2-beappdb1-credentials"
 }
 
+resource "aws_secretsmanager_secret" "aws_credentials_infra" {
+    name = "aws-credentials-infra"
+}
+
 resource "aws_secretsmanager_secret_version" "rds_west2_beappdb1_credentials_version" {
     secret_id = aws_secretsmanager_secret.rds_west2_beappdb1_credentials.id
     secret_string = <<EOF
@@ -24,10 +28,17 @@ EOF
 data "aws_secretsmanager_secret" "rds_west2_beappdb1_data" {
   arn = aws_secretsmanager_secret.rds_west2_beappdb1_credentials.arn
 }
- 
+
+data "aws_secretsmanager_secret" "aws_credentials_infra_data" {
+  arn = aws_secretsmanager_secret.aws_credentials_infra.arn
+}
+
 # Importing the AWS secret version created previously using arn.
  
 data "aws_secretsmanager_secret_version" "rds_west2_beappdb1_version_data" {
   secret_id = data.aws_secretsmanager_secret.rds_west2_beappdb1_data.name
 }
  
+data "aws_secretsmanager_secret_version" "aws_credentials_infra_version_data" {
+  secret_id = data.aws_secretsmanager_secret.aws_credentials_infra_data.name
+}
